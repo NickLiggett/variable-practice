611961a342416ea64eb5f50dc23c43e4807bdce6
Nick Liggett <nick.liggett@gmail.com>
2022-04-13 20:35:34 -0600

    Attempt to push

---
+++ variables.js
@@ -0,0 +1,11 @@
+var firstName = "Nick"
+var lastName = "Liggett"
+// This will result in my first and last name.
+
+var age = 31
+var height = 6.123
+// Here you have some basic info like age and height
+
+var hasHair = true
+var canJavaScript = true
+// One is actually an opinion, but let me have this.
